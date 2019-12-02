e63cf2c9fe9d80d56aad17c90af8d388c6c9e4c2
zyf_HW13 <921233440@qq.com>
2019-12-02 16:56:18 +0800

    modify

--- src/main/java/com/github/security/authcatication/JwtRefreshSuccessHandler.java
+++ src/main/java/com/github/security/authcatication/JwtRefreshSuccessHandler.java
@@ -13,7 +13,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserCache;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.cache.NullUserCache;
